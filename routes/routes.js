ba9185c3ff15a3633c77d7c36f3c4ed3acd193cc
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-01-16 19:42:07 +0100

    Test de CORS pour la prod

--- routes/routes.js
+++ routes/routes.js
@@ -20,7 +20,7 @@ const currentUser = require("../middlewares/currentUser");
 const stripeWebhook = require("../webhooks/stripe");
 
 const corsOptions = {
-  origin: "http://front:80",
+  origin: "http://localhost",
 };
 
 module.exports = function (app) {
