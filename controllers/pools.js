47bf9ad69a5d39da4bb20c832f8ac20b2b320d95
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-01-30 17:18:24 +0100

    Amélioration de poules / résultats

--- controllers/pools.js
+++ controllers/pools.js
@@ -49,7 +49,7 @@ module.exports = class PoolsController extends CRUDController {
       .sort("rank")
       .limit(8);
 
-    res.send(pools.concat(otherBestRiders));
+    res.send(pools);
   };
 
   createPools = async (req, res) => {
@@ -123,30 +123,18 @@ module.exports = class PoolsController extends CRUDController {
 
     const step = await Step.findById(stepId);
 
-    await req.body.poolsEntries.forEach(async (poolEntry) => {
+    for (const poolEntry of req.body.poolsEntries) {
       const pool = await Pool.findById(poolEntry._id);
-      if (!pool) return res.status(404).send("Pool not found");
+      if (!pool) {
+        return res.status(404).send("Pool not found");
+      }
       pool.score = poolEntry.score;
       await pool.save();
-    });
+    }
 
     await this.updatePoolRank(stepId, step.ridersQualifiedCount);
 
     res.send(await this.getPoolInDb(stepId));
-  };
-
-  getPoolInDb = async (stepId) => {
-    const pools = await this.model
-      .find({ step: stepId })
-      .populate({
-        path: "registration", // Chemin vers le champ 'registration'
-        populate: {
-          path: "rider", // Chemin à partir de 'registration' vers 'rider'
-          model: "Riders", // Nom du modèle pour 'rider', si nécessaire
-        },
-      })
-      .sort("poolNumber");
-    return pools;
   };
 
   updatePoolRank = async (stepId, qualifiedNumber) => {
@@ -163,6 +151,20 @@ module.exports = class PoolsController extends CRUDController {
       pools[i].rank = i + 1; // Le rang commence à 1
       await pools[i].save();
     }
+  };
+
+  getPoolInDb = async (stepId) => {
+    const pools = await this.model
+      .find({ step: stepId })
+      .populate({
+        path: "registration", // Chemin vers le champ 'registration'
+        populate: {
+          path: "rider", // Chemin à partir de 'registration' vers 'rider'
+          model: "Riders", // Nom du modèle pour 'rider', si nécessaire
+        },
+      })
+      .sort("poolNumber");
+    return pools;
   };
 
   publishResult = async (req, res) => {
