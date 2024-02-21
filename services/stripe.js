677e23ca15bb51e9cebf4fc178b95198fae436b1
Anthony CHIRON <74792178+AnthonyChiron@users.noreply.github.com>
2024-02-21 21:59:33 +0100

    Ajout du refund

--- services/stripe.js
+++ services/stripe.js
@@ -47,3 +47,16 @@ module.exports.refusePaymentIntent = async function (paymentIntentId) {
     console.error("Erreur lors du refus du paiement", error);
   }
 };
+
+module.exports.refundPaymentIntent = async function (paymentIntentId) {
+  try {
+    // Effectuez le remboursement
+    await stripe.refunds.create({
+      payment_intent: paymentIntentId,
+    });
+
+    console.log("Paiement remboursé avec succès");
+  } catch (error) {
+    console.error("Erreur lors du remboursement du paiement", error);
+  }
+};
